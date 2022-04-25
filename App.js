af0dae1ef2262b6d5993abfb520b29de3e3a2189
Nthati Sello <nthatijosephinasello@gmail.com>
2022-04-25 23:21:40 +0200

    architecture.jpg

--- App.js
+++ App.js
@@ -1,186 +1,318 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Fontisto } from '@expo/vector-icons';
+import { StyleSheet, View, Text ,ScrollView, Image } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+     <View style={styles.shape0}>
+       <View style={styles.menu}><Ionicons name="menu" size={30} color="white" /></View>
+       <View style={styles.left}><AntDesign name="down" size={24} color="white" /></View>
+       <Text style={{color: 'white', fontWeight: '700', fontSize: 20}}>Virtual Classes</Text>
+       </View> 
       
-      
-      <View style={styles.rec1}>
-        <View style={styles.left}><AntDesign name="left" size={26} color="white" /></View>
-      <View style={styles.rec0}><AntDesign name="search1" size={20} color="white" /></View>
-      <View style={styles.option}><Ionicons name="options-outline" size={26} color="white" /></View>
-      </View>
+      <ScrollView>
+       <View style={styles.shape1}>
+       <View style={styles.school}><FontAwesome5 name="school" size={40} color="white" /></View>
+       <Text style={{color: 'white', fontWeight: '700', fontSize: 24}}>Student Courses</Text>
+       </View> 
 
-      <View style={styles.rec2}><View style={styles.down}><AntDesign name="down" size={20} color="grey" /></View><Image
-      source={require("./assets/fake.jpg")}
-      style={{height: 70, width: 70, top: 29, marginBottom: 35}} 
-      /><View styles={styles.listen}><Text style={{fontWeight: '700'}}>Real Love is still there, you just need to find the right person to remind you that.</Text></View>
-      <View style={styles.molupe}><Text style ={{fontWeight: '700'}}>Molupe Sello</Text></View>
-      <View style={styles.yesterday}><Text style={{color: 'grey'}}>Yesterday  <Fontisto name="world" size={12} color="lightgrey" /></Text></View>
-      </View>
-        
-      <View style={styles.rec3}><Image
-      source={require("./assets/love.jpeg")}
-      style={{height: 260, width: 350,}} 
-      /></View>
-      <View style={styles.rec4}>
-        <Text style = {{color: 'grey'}}>50 Likes       100 comments</Text>
-      </View>
+       <View style={styles.shape2}>
+       <View style={styles.left}><AntDesign name="down" size={24} color="white" /></View>
+       <Text style={{color: 'white', fontWeight: '700', fontSize: 17,}}>Software Engineering & Multimedia</Text>
+       </View>
 
-      <View style={styles.rec5}>
-      <View style={styles.like}><AntDesign name="like1" size={24} color="lightgrey" /><Text style = {{color: 'grey'}}>Like</Text></View>
-      <View style={styles.comment}><FontAwesome name="comment" size={24} color="lightgrey" /><Text style = {{color: 'grey'}}>Comment</Text></View>
-      <View style={styles.share}><MaterialCommunityIcons name="share" size={24} color="lightgrey" /><Text style = {{color: 'grey'}}>Share</Text></View>  
-      </View>
-
-      <View style={styles.rec6}>
-        <Image
-      source={require("./assets/nthati.jpg")}
-      style={{height: 70, width: 70, top: 18}} 
+       <View style={styles.rec1}>
+       <Image
+      source={require("./assets/software.jpg")}
+      style={{height: 250, width: 344,borderTopLeftRadius:10, borderTopRightRadius: 10  }} 
       />
-      <View style= {styles.nthati}><Text style= {{color: 'black', fontWeight: '700'}}>Nthati Sello</Text></View>
-      <View style={styles.day}><Text style = {{color: 'grey'}}>5 April  <Fontisto name="world" size={12} color="lightgrey" /></Text></View>
-      <View style={styles.down}><AntDesign name="down" size={20} color="grey" /></View>
-      </View>
+       </View>
+
+       <View style={styles.rec2}>
+         <Text style={{color: 'white', fontSize: 16, fontWeight: '600', textAlign: 'justify'}}>Software Engineering is a detailed study of engineering to the design, development and maintenance of software.  Multimedia is a representation of information in an interactive manner with the use of a combination of text, audio, video and photos.</Text>
+       </View>
+
+       <View style={styles.shape3}>
+       <View style={styles.left}><AntDesign name="down" size={24} color="white" /></View>
+       <Text style={{color: 'white', fontWeight: '700', fontSize: 17,}}>Information Technology</Text>
+       </View>
+
+       <View style={styles.rec3}>
+       <Image
+      source={require("./assets/it.jpg")}
+      style={{height: 250, width: 344,borderTopLeftRadius:10, borderTopRightRadius: 10  }} 
+      />
+       </View>
+
+       <View style={styles.rec4}>
+         <Text style={{color: 'white', fontSize: 16, fontWeight: '600', textAlign: 'justify'}}>Information Technology is the use of any computers, storage, networking and other physical devices, infrastructure and processes to create, process, store, secure and exchange all forms of electronic data. Communication is also faster with IT.</Text>
+       </View>
+
+
+       <View style={styles.shape4}>
+       <View style={styles.left}><AntDesign name="down" size={24} color="white" /></View>
+       <Text style={{color: 'white', fontWeight: '700', fontSize: 17,}}>Architecture and Build Environment</Text>
+       </View>
+
+       <View style={styles.rec5}>
+       <Image
+      source={require("./assets/architecture.jpg")}
+      style={{height: 250, width: 344,borderTopLeftRadius:10, borderTopRightRadius: 10  }} 
+      />
+       </View>
+
+       <View style={styles.rec6}>
+         <Text style={{color: 'white', fontSize: 16, fontWeight: '600', textAlign: 'justify'}}>Architecture is both the process and the product of planning, designing, and constructing buildings or other structures. Architectural works, in the material form of buildings, are often perceived as cultural symbols and as works of art.</Text>
+       </View>
+
+       <View style={styles.logout}>
+         <Text style={{color: 'white', fontSize: 17, fontWeight: '700'}}> Do you wish to Logout?</Text>
+         <View style={styles.left}><AntDesign name="down" size={24} color="white" /></View>
+         
+       </View>
+
+       </ScrollView>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgrey',
+    backgroundColor: 'teal',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 30 
   },
 
-  rec1:{
-    backgroundColor:'royalblue',
-    height:80,
-    width: 360,
-    alignItems: 'flex-start',
-    marginBottom: 8,
-    justifyContent: 'center',  
-  }, 
-
-  rec0:{
-    height:30,
-    width: 250,
+  shape0: {
+    height: 60,
+    width: 358,
     backgroundColor: 'dodgerblue',
-    borderBottomLeftRadius: 5,
-    borderBottomRightRadius: 5,
-    borderTopRightRadius: 5,
-    borderTopLeftRadius: 5,
-    right: -50,
-    top: -7,
-    padding: 4,
-  },
-
-  rec2:{
-    backgroundColor: 'white',
-    height: 140,
-    width: 344,
     alignItems: 'flex-start',
-    justifyContent: 'center',
-    padding: 10
-  },
+    justifyContent: 'space-evenly',
+    paddingLeft: 60,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+      },
   
-  rec3:{
-    backgroundColor: 'white',
-    height: 260,
-    width: 350,  
-    alignItems: 'center'
-  },
-
-  rec4:{
-    backgroundColor: 'white',
-    height: 40,
-    width: 344,
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'flex-start'
-  },
-
-  rec5:{
-    backgroundColor: 'whitesmoke',
-    height: 40,
-    width: 344,
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-    marginBottom: 8,  
-  },
-
-  like:{
-    justifyContent: 'space-between',
+  shape1: {
+    width: 340,
+    height: 130,
     alignItems: 'center',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    marginRight: 45,  
+    paddingTop: 10,
+    justifyContent: 'space-evenly',
+    marginBottom: 30
   },
 
-  comment:{
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    marginRight: 30,
-  },
-
-  share:{
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    alignItems: 'flex-start', 
-    marginRight: 35 
-  },
-
-  rec6:{
-    backgroundColor: 'white',
-    height: 95,
-    width: 344,
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-  },
-
-  down:{
+  menu: {
     position: 'absolute',
-    right: 5,
-    top: 8
-  },
-  nthati:{
-    top:-50,
-    right: -80, 
-  },
-  molupe:{
-    top:-100,
-    right: -80,
-  },
-  yesterday:{
-    top:-100,
-    right: -80,
-  },
-  day:{
-    top:-50,
-    right: -80, 
-  },
-  option:{
-    position: 'absolute',
-    right: 7,
-    top:35
-  },
-  left:{
+    top: 15,
+    paddingLeft: 15
 
-top: 20
-  }
+  },
+
+  left: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+  },
+
+  shape2: {
+    height: 60,
+    width: 344,
+    backgroundColor: 'dodgerblue',
+    alignItems: 'flex-start',
+    justifyContent: 'space-evenly',
+    paddingLeft: 15,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+
+      },
+
+  shape3: {
+    height: 60,
+    width: 344,
+    backgroundColor: 'dodgerblue',
+    alignItems: 'flex-start',
+    justifyContent: 'space-evenly',
+    paddingLeft: 15,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+    width: 0,
+    height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+  },
+
+  shape4: {
+    height: 60,
+    width: 344,
+    backgroundColor: 'dodgerblue',
+    alignItems: 'flex-start',
+    justifyContent: 'space-evenly',
+    paddingLeft: 15,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+      },
+
+  rec1: {
+    backgroundColor: 'white',
+    height: 250,
+    width: 344,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+
+  rec2: {
+    backgroundColor: 'deepskyblue',
+    height: 120,
+    width: 344,
+    marginBottom: 30,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+      },
+    
+  rec3: {
+    backgroundColor: 'white',
+    height: 250,
+    width: 344,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+
+  rec4: {
+    backgroundColor: 'deepskyblue',
+    height: 120,
+    width: 344,
+    marginBottom: 30,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+      },
+  
+  rec5: {
+    backgroundColor: 'white',
+    height: 250,
+    width: 344,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+
+  rec6: {
+    backgroundColor: 'deepskyblue',
+    height: 120,
+    width: 344,
+    marginBottom: 30,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+      },
+
+  logout: {
+    height: 60,
+    width: 344,
+    backgroundColor: 'dodgerblue',
+    alignItems: 'flex-start',
+    justifyContent: 'space-evenly',
+    paddingLeft: 15,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    marginBottom: 30,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+      },
+  
   
 });
